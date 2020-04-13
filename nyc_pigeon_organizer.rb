690474b8996d9c8a05d72d9680a5be56661255af
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-13 17:43:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,1 +1,19 @@
 def nyc_pigeon_organizer(data)
+  pigeon_names = Hash.new
+  data.each do |key, value|
+    value.each do |key, name|
+      name.each do |name|
+
+        if !pigeon_names[name]
+          pigeon_names[name] = {}
+        end
+
+        if !pigeon_names[name][key]
+            pigeon_names[name][key] = []
+        end
+        pigeon_names[name][key] << key.to_s
+      end
+    end
+  end
+  pigeon_names
+end
